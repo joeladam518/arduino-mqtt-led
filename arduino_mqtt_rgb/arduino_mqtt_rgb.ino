082018618083ef088b1cc0891c0ffc7b75cb3374
Joel Haker <joeladam@gmail.com>
2019-07-07 21:03:47 -0400

    Updates to use arduino json 6, publishes status on update, adds third twinkle light pin

--- arduino_mqtt_rgb/arduino_mqtt_rgb.ino
+++ arduino_mqtt_rgb/arduino_mqtt_rgb.ino
@@ -28,6 +28,7 @@ static const int BLUE_PIN = 6;
 /* Twinkle Light Pins */
 static const int TW1 = 44;
 static const int TW2 = 45;
+static const int TW3 = 46;
 
 /* RGB LED Enum */
 static const int R = 0;
@@ -66,29 +67,7 @@ int currentcolor[3] = { 0, 0, 0 };
 /* RGB functions */
 
 // Sets an RGB color
-void setRGB() 
-{
-    // Default is white
-    int color[3] = { 200, 100, 25 };
-
-    if (color[R] < 0) { color[R] = 0; }
-    if (color[R] > 255) { color[R] = 255; }
-    if (color[G] < 0) { color[G] = 0; }
-    if (color[G] > 255) { color[G] = 255; }
-    if (color[B] < 0) { color[B] = 0; }
-    if (color[B] > 255) { color[B] = 255; }
-
-    analogWrite(RED_PIN, color[R]);
-    analogWrite(GREEN_PIN, color[G]);
-    analogWrite(BLUE_PIN, color[B]);
-
-    currentcolor[R] = color[R];
-    currentcolor[G] = color[G];
-    currentcolor[B] = color[B];
-}
-
-// Overloaded setRGB() method
-void setRGB(int color[3]) 
+void setRGB(int color[3])
 {
     if (color[R] < 0) { color[R] = 0; }
     if (color[R] > 255) { color[R] = 255; }
@@ -107,7 +86,7 @@ void setRGB(int color[3])
 }
 
 // Controls a smooth lighting fade
-void fade(int startcolor[], int endcolor[], int fadeTime) 
+void fade(int startcolor[], int endcolor[], int fadeTime)
 {
     int color[3];
 
@@ -115,11 +94,11 @@ void fade(int startcolor[], int endcolor[], int fadeTime)
         color[R] = map(t, 0, fadeTime, startcolor[R], endcolor[R]);
         color[G] = map(t, 0, fadeTime, startcolor[G], endcolor[G]);
         color[B] = map(t, 0, fadeTime, startcolor[B], endcolor[B]);
-        
+
         setRGB(color);
         delay(1);
     }
-    
+
     setRGB(endcolor);
     delay(1);
 }
@@ -136,30 +115,33 @@ void togglePin(int pin)
 void togglePin(int pin, char* data)
 {
     if (
-        strcmp(data, "1") == 0 
+        strcmp(data, "1") == 0
     ||  strcmp(data, "on") == 0
     ||  strcmp(data, "ON") == 0
     ) {
         digitalWrite(pin, HIGH);
     } else if (
-        strcmp(data, "0") == 0 
-    ||  strcmp(data, "off") == 0 
+        strcmp(data, "0") == 0
+    ||  strcmp(data, "off") == 0
     ||  strcmp(data, "OFF") == 0
     ) {
         digitalWrite(pin, LOW);
     }
 }
 
-void handleRGBTopic(char* data) 
+void handleRGBTopic(char* data)
 {
-    // The json buffer 
-    StaticJsonBuffer<256> jsonBuffer;
+    // The json buffer
+    StaticJsonDocument<256> doc;
 
     // Parse the json object
-    JsonObject& root = jsonBuffer.parseObject(data);
+    DeserializationError error = deserializeJson(doc, data);
 
-    if (!root.success()) {
-        Serial.println("parseObject() failed");
+    if (error) {
+        Serial.println("DeserializationError!");
+        Serial.print("Error: ");
+        Serial.println(error.c_str());
+        return;
     }
 
     // The current color to fade from
@@ -170,12 +152,12 @@ void handleRGBTopic(char* data)
 
     // The new color to fade to
     int newcolor[3];
-    newcolor[R] = root["r"].as<int>();
-    newcolor[G] = root["g"].as<int>();
-    newcolor[B] = root["b"].as<int>();
+    newcolor[R] = doc["r"];
+    newcolor[G] = doc["g"];
+    newcolor[B] = doc["b"];
 
     // The speed at which to change the colors
-    int time = root["time"].as<int>(); 
+    int time = doc["time"];
 
     if (!time) {
         // If there is no "time" in the json string just set the color
@@ -186,17 +168,56 @@ void handleRGBTopic(char* data)
     }
 }
 
+void publishRGBStatus()
+{
+    const int capacity = JSON_OBJECT_SIZE(3);
+    char output[128];
+    
+    StaticJsonDocument<capacity> doc;
+
+    doc["r"] = currentcolor[R];
+    doc["g"] = currentcolor[G];
+    doc["b"] = currentcolor[B];
+
+    serializeJson(doc, output, sizeof(output));
+
+    client.publish("stat/arduino/mega/rgb", output);
+}
+
+void publishPinStatus(char* topic_postfix, int pin)
+{
+    char topic[30] = "stat/arduino/mega/";
+    strcat(topic, topic_postfix);
+    
+    if (digitalRead(pin)) {
+        client.publish(topic, "on");
+    } else {
+        client.publish(topic, "off");
+    }
+}
+
+void publishDeviceStatus()
+{
+    Serial.println("Asked for device status!");
+
+    publishRGBStatus();
+    publishPinStatus("pwr", PWR_PIN);
+    publishPinStatus("tw1", TW1);
+    publishPinStatus("tw2", TW2);
+    publishPinStatus("tw3", TW3);
+}
+
 //--------------------------------------------------------------------------------------------------
 /* MQTT functions */
 
-// The PubSubClient callback function 
-void callback(char* topic, byte* payload, unsigned int length) 
+// The PubSubClient callback function
+void callback(char* topic, byte* payload, unsigned int length)
 {
     //Serial.println("Message arrived!");
     //debug_topic(topic);
     //debug_payload(payload, length);
 
-    char buffer[100]; // payload buffer
+    char buffer[128]; // payload buffer
     memset(buffer, 0, sizeof(buffer)); // empty the buffer
 
     for (int i=0; i<length; i++) {
@@ -204,22 +225,34 @@ void callback(char* topic, byte* payload, unsigned int length)
     }
 
     //debug_buffer(buffer);
-    
-    if (strcmp(topic, "arduino/mega/pwr") == 0) {
-        
-        togglePin(PWR_PIN, buffer);
 
-    } else if (strcmp(topic, "arduino/mega/rgb") == 0) {
+    if (strcmp(topic, "cmd/arduino/mega/rgb") == 0) {
 
         handleRGBTopic(buffer);
 
-    } else if (strcmp(topic, "arduino/mega/tw1") == 0) {
-            
-        togglePin(TW1, buffer);
-        
-    } else if (strcmp(topic, "arduino/mega/tw2") == 0) {
-            
-        togglePin(TW2, buffer);
+    } else if (strcmp(topic, "cmd/arduino/mega/pwr") == 0) {
+
+        togglePin(PWR_PIN, buffer);
+        publishPinStatus("pwr", PWR_PIN);
+
+    } else if (strcmp(topic, "cmd/arduino/mega/tw1") == 0) {
+
+        togglePin(TW1);
+        publishPinStatus("tw1", TW1);
+
+    } else if (strcmp(topic, "cmd/arduino/mega/tw2") == 0) {
+
+        togglePin(TW2);
+        publishPinStatus("tw2", TW2);
+
+    } else if (strcmp(topic, "cmd/arduino/mega/tw3") == 0) {
+
+        togglePin(TW3);
+        publishPinStatus("tw3", TW3);
+
+    } else if (strcmp(topic, "cmd/arduino/mega/status") == 0) {
+
+        publishDeviceStatus();
 
     } else {
         Serial.print("Could not handle topic: \"");
@@ -228,7 +261,7 @@ void callback(char* topic, byte* payload, unsigned int length)
     }
 }
 
-void reconnect() 
+void reconnect()
 {
     // Loop until we're reconnected
     while (!client.connected()) {
@@ -239,10 +272,12 @@ void reconnect()
             // Once connected, publish an announcement...
             client.publish("confirm", "arduinoLED has connected to the broker");
             // ... and resubscribe
-            client.subscribe("arduino/mega/pwr");
-            client.subscribe("arduino/mega/rgb");
-            client.subscribe("arduino/mega/tw1");
-            client.subscribe("arduino/mega/tw2");
+            client.subscribe("cmd/arduino/mega/rgb");
+            client.subscribe("cmd/arduino/mega/pwr");
+            client.subscribe("cmd/arduino/mega/tw1");
+            client.subscribe("cmd/arduino/mega/tw2");
+            client.subscribe("cmd/arduino/mega/tw3");
+            client.subscribe("cmd/arduino/mega/status");
         } else {
             Serial.print("failed, rc=");
             Serial.print(client.state());
@@ -285,13 +320,16 @@ void setup()
     digitalWrite(TW1, LOW);
     pinMode(TW2, OUTPUT);
     digitalWrite(TW2, LOW);
+    pinMode(TW3, OUTPUT);
+    digitalWrite(TW3, LOW);
 
-    // Set the default values for the powers switch 
+    // Set the default values for the powers switch
     pinMode(PWR_PIN, OUTPUT);
     digitalWrite(PWR_PIN, LOW);
 
-    // Set the default values for the the RGB lights
-    setRGB();
+    // Set the default values for the the RGB lights (white)
+    int white[3] = { 200, 100, 25 };
+    setRGB(white);
 }
 
 void loop()
